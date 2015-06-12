68e2c13aaba6afa13beb8bf2734793cb1532270c
leroi <leroidejesa@gmail.com>
2015-06-12 11:30:15 -0700

    add ability to ask a question from front page

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 Embera.Router.map(function() {
   this.resource('questions', {path: '/'});
-  this.resource('question', {path: ':post_id'}, function() {
+  this.resource('question', {path: ':question_id'}, function() {
     this.resource('new-answer');
   });
   this.resource('new-question');
